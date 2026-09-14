526637682c8676457b392505820e7b936acf4d15
habeebahmad215-star <habeebahmad215@gmail.com>
2026-09-14 07:37:30 +0530

    Add professional resize and rotate handles to selected canvas elements

--- lib/widgets/design_canvas.dart
+++ lib/widgets/design_canvas.dart
@@ -1,8 +1,9 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import '../models/design_models.dart';
 import '../state/workspace_controller.dart';
 
-class DesignCanvas extends StatelessWidget {
+class DesignCanvas extends StatefulWidget {
   final WorkspaceController controller;
   final GlobalKey repaintKey;
 
@@ -13,11 +14,21 @@ class DesignCanvas extends StatelessWidget {
   });
 
   @override
+  State<DesignCanvas> createState() => _DesignCanvasState();
+}
+
+class _DesignCanvasState extends State<DesignCanvas> {
+  double? _rotationStartAngle;
+  double? _rotationStartValue;
+
+  WorkspaceController get controller => widget.controller;
+
+  @override
   Widget build(BuildContext context) {
     final page = controller.page;
 
     return RepaintBoundary(
-      key: repaintKey,
+      key: widget.repaintKey,
       child: SizedBox(
         width: page.size.width,
         height: page.size.height,
@@ -27,6 +38,7 @@ class DesignCanvas extends StatelessWidget {
           child: DecoratedBox(
             decoration: BoxDecoration(color: page.background),
             child: Stack(
+              clipBehavior: Clip.none,
               children: [for (final e in page.elements) _element(e)],
             ),
           ),
@@ -80,42 +92,181 @@ class DesignCanvas extends StatelessWidget {
       height: e.height,
       child: Transform.rotate(
         angle: e.rotation,
-        child: GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onTap: () => controller.select(e.id),
-          onPanStart: e.locked
-              ? null
-              : (_) {
-                  controller.select(e.id);
-                  controller.startContinuousEdit();
-                },
-          onPanUpdate: e.locked
-              ? null
-              : (details) {
-                  controller.moveSelectedBy(
-                    details.delta.dx,
-                    details.delta.dy,
-                  );
-                },
-          onPanEnd: e.locked
-              ? null
-              : (_) => controller.finishContinuousEdit(),
-          child: DecoratedBox(
-            decoration: selected
-                ? BoxDecoration(
-                    border: Border.all(
-                      color: const Color(0xFF7C3AED),
-                      width: 3,
-                    ),
-                  )
-                : const BoxDecoration(),
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: child,
+        alignment: Alignment.center,
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () => controller.select(e.id),
+              onPanStart: e.locked
+                  ? null
+                  : (_) {
+                      controller.select(e.id);
+                      controller.startContinuousEdit();
+                    },
+              onPanUpdate: e.locked
+                  ? null
+                  : (details) {
+                      controller.moveSelectedBy(
+                        details.delta.dx,
+                        details.delta.dy,
+                      );
+                    },
+              onPanEnd: e.locked
+                  ? null
+                  : (_) => controller.finishContinuousEdit(),
+              child: DecoratedBox(
+                decoration: selected
+                    ? BoxDecoration(
+                        border: Border.all(
+                          color: const Color(0xFF7C3AED),
+                          width: 3,
+                        ),
+                      )
+                    : const BoxDecoration(),
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: child,
+                ),
+              ),
+            ),
+            if (selected && !e.locked) _selectionHandles(e),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _selectionHandles(DesignElement e) {
+    return Positioned.fill(
+      child: IgnorePointer(
+        ignoring: false,
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            _handle(e, 'top-left', Alignment.topLeft),
+            _handle(e, 'top-right', Alignment.topRight),
+            _handle(e, 'bottom-left', Alignment.bottomLeft),
+            _handle(e, 'bottom-right', Alignment.bottomRight),
+            _rotationHandle(e),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _handle(DesignElement e, String type, Alignment alignment) {
+    return Align(
+      alignment: alignment,
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onPanStart: (_) {
+          controller.select(e.id);
+          controller.startContinuousEdit();
+        },
+        onPanUpdate: (details) {
+          controller.resizeSelectedFromHandle(
+            type,
+            details.delta.dx,
+            details.delta.dy,
+          );
+        },
+        onPanEnd: (_) => controller.finishContinuousEdit(),
+        child: Container(
+          width: 30,
+          height: 30,
+          alignment: Alignment.center,
+          child: Container(
+            width: 18,
+            height: 18,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: const Color(0xFF7C3AED), width: 3),
+              borderRadius: BorderRadius.circular(5),
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 5,
+                  spreadRadius: 1,
+                  color: Colors.black26,
+                ),
+              ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _rotationHandle(DesignElement e) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Transform.translate(
+        offset: const Offset(0, -42),
+        child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onPanStart: (details) {
+            controller.select(e.id);
+            controller.startContinuousEdit();
+            final center = _globalCenter(e);
+            _rotationStartAngle = math.atan2(
+              details.globalPosition.dy - center.dy,
+              details.globalPosition.dx - center.dx,
+            );
+            _rotationStartValue = e.rotation;
+          },
+          onPanUpdate: (details) {
+            final startAngle = _rotationStartAngle;
+            final startValue = _rotationStartValue;
+            if (startAngle == null || startValue == null) return;
+            final center = _globalCenter(e);
+            final currentAngle = math.atan2(
+              details.globalPosition.dy - center.dy,
+              details.globalPosition.dx - center.dx,
+            );
+            var delta = currentAngle - startAngle;
+            if (delta > math.pi) delta -= math.pi * 2;
+            if (delta < -math.pi) delta += math.pi * 2;
+            controller.rotateSelectedTo(startValue + delta);
+          },
+          onPanEnd: (_) {
+            _rotationStartAngle = null;
+            _rotationStartValue = null;
+            controller.finishContinuousEdit();
+          },
+          child: Container(
+            width: 34,
+            height: 34,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+              border: Border.all(color: const Color(0xFF7C3AED), width: 3),
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 5,
+                  spreadRadius: 1,
+                  color: Colors.black26,
+                ),
+              ],
+            ),
+            child: const Icon(
+              Icons.rotate_right_rounded,
+              size: 21,
+              color: Color(0xFF7C3AED),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Offset _globalCenter(DesignElement e) {
+    final box = widget.repaintKey.currentContext?.findRenderObject();
+    if (box is RenderBox) {
+      return box.localToGlobal(
+        Offset(e.x + e.width / 2, e.y + e.height / 2),
+      );
+    }
+    return Offset(e.x + e.width / 2, e.y + e.height / 2);
   }
 }
